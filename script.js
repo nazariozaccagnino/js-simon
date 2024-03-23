bd59bfa1f0e3374d0ffcf270f67237128db146eb
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-23 16:36:20 +0100

    printed in page the numbers of array

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-const casualNumbers = [];
 
-for (let k = 1; k <= 5; k++){
-    let number = getRndInteger(1, 100);
-    if (number != casualNumbers){
-        casualNumbers.push(number)
+const startGame = document.getElementById('startgame');
+
+startGame.addEventListener ('click', function (){
+    const casualNumbers = [];
+    for (let k = 1; k <= 5; k++){
+        let number = getRndInteger(1, 100);
+        if (number != casualNumbers.value){
+            casualNumbers.push(number)
+        }
+    }console.log(casualNumbers, 'array dei numeri casuali');
+
+    casualNumbers.forEach((element) => {
+       const div = document.createElement("div");
+        div.innerHTML = `${element}`;
+        document.body.appendChild(div);
     }
-}console.log(casualNumbers)
+    );
+
+    
+})
+
+
